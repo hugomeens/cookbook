0f9ab485b3637fbe5791587799a4fe4147fa49df
Hugo Meens <hugo.meens@epita.fr>
2023-04-20 10:27:15 +0200

    mongodb global connection

---
+++ server/dao/db.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+
+class Database {
+    constructor(_url) {
+        this.url = _url;
+        this.client;
+        this.db;
+    }
+
+    async open() {
+        this.client = await MongoClient.connect(this.url);
+        this.db = this.client.db("cookBook");
+        return this.db;
+    }
+
+    async collection(collection) {
+        if (!this.db) await this.open();
+        return this.db.collection(collection);
+    }
+}
+
+const db = new Database('mongodb://127.22.0.3:27017');
+
+module.exports = { db };
