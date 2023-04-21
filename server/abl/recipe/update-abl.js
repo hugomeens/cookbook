62298ce3c6ecf8e7a3ec06288722ce2dbd680050
Samuel PERRIER <samuel.perrier@epita.fr>
2023-04-21 19:03:55 +0200

    update recipe

--- server/abl/recipe/update-abl.js
+++ server/abl/recipe/update-abl.js
@@ -1,31 +1,30 @@
 const path = require('path');
 const { recipeDao } = require('../../dao/recipe-dao');
-const { listRecipeSchema } = require('../../schemas/recipe-schema');
+const { updateRecipeSchema } = require('../../schemas/recipe-schema');
 const Ajv = require('ajv').default;
 const { statusCodes } = require('../../utils/statusCodes');
 
-async function UpdateAbl(req, res) {
-    try {
-      const ajv = new Ajv();
-      let recipe = req.body;
-      const valid = ajv.validate(schema, recipe);
-      if (valid) {
-        recipe = await dao.updateRecipeSchema(recipe);
-        res.json(recipe);
-      } else {
-        res.status(400).json({
-          errorMessage: "validation of input failed",
-          params: recipe,
-          reason: ajv.errors,
-        });
-      }
-    } catch (e) {
-      if (e.message.startsWith("recipe with given id")) {
-        res.status(400).json({ error: e.message });
-      }
-      res.status(500).json(e);
-    }
+async function UpdateAbl(body, res) {
+  const ajv = new Ajv();
+  const valid = ajv.validate(updateRecipeSchema, body);
+
+  if (!valid) {
+    return res.status(statusCodes.BAD_REQUEST).json({ error: ajv.errors });
   }
-  
-  module.exports = UpdateAbl;
-  
+
+  try {
+    const id = body.id;
+    delete body.id;
+    const mongoRes = await recipeDao.update(id, body);
+    if (mongoRes.matchedCount == 0) {
+      res.status(statusCodes.NOT_FOUND).json({ error: 'Recipe not found.' });
+    } else {
+      res.status(statusCodes.OK).json({ _id: id });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ error: e });
+  }
+}
+
+module.exports = UpdateAbl;
