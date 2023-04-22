11bebd5f0ff8cdf9c77a4018f8ced8a4c91a8997
Hugo Meens <hugo.meens@epita.fr>
2023-04-22 17:28:36 +0200

    create ingredient view update

--- client/src/pages/ingredients/create.jsx
+++ client/src/pages/ingredients/create.jsx
@@ -1,50 +1,17 @@
-import { Modal, Text, Select, TextInput, Button, FileInput, Group, Image } from '@mantine/core';
-import { isNotEmpty, useForm } from '@mantine/form';
-import { useRef } from 'react';
+import { Modal, Text } from '@mantine/core';
+import IngredientViewUpdate from './ingredient-view-update';
 import API from '../../services/api';
 
 const ModalCreateIngredient = ({ opened, handler, addIngredient }) => {
-    const handleClose = () => {
-        form.reset();
+
+    const validated = (item) => {
+        console.log("validated")
+        addIngredient(item);
         handler();
-    };
-
-    const button = useRef(null);
-
-    const form = useForm({
-        initialValues: {
-            name: '',
-            unit: '',
-            image: '',
-            alternativeNames: '',
-        },
-        validate: {
-            name: isNotEmpty('Name is required'),
-            unit: isNotEmpty('Unit is required'),
-        },
-    });
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (form.validate().hasErrors) return;
-        try {
-            button.current.loading = true;
-            form.values.alternativeNames =
-                form.values.alternativeNames.length > 0 ? form.values.alternativeNames.split(';') : [];
-            delete form.values.image;
-            form.values.imageId = '';
-            const res = await API.createIngredient(form.values);
-            addIngredient(res.data);
-            button.current.loading = false;
-            handleClose();
-        } catch (error) {
-            button.current.loading = false;
-            // todo
-        }
-    };
+    }
 
     return (
-        <Modal.Root opened={opened} onClose={handleClose} size="xs">
+        <Modal.Root opened={opened} onClose={handler} size="xs">
             <Modal.Overlay />
             <Modal.Content>
                 <Modal.Header>
@@ -56,50 +23,7 @@ const ModalCreateIngredient = ({ opened, handler, addIngredient }) => {
                     <Modal.CloseButton />
                 </Modal.Header>
                 <Modal.Body>
-                    <form onSubmit={handleSubmit}>
-                        <TextInput
-                            label="Name"
-                            placeholder="Name"
-                            withAsterisk
-                            {...form.getInputProps('name')}
-                            mb="sm"
-                        />
-                        <TextInput
-                            label="Alternative names"
-                            placeholder="Alternative names"
-                            description="Comma separated"
-                            {...form.getInputProps('alternativeNames')}
-                            mb="sm"
-                        />
-                        <FileInput label="Image" placeholder="Select image" {...form.getInputProps('image')} mb="md" />
-                        <Image
-                            src={form.values.image}
-                            alt={form.values.name}
-                            withPlaceholder
-                            height={160}
-                            radius="sm"
-                        />
-                        <Select
-                            label="Unit"
-                            placeholder="Select unit"
-                            withAsterisk
-                            dropdownPosition="bottom"
-                            {...form.getInputProps('unit')}
-                            data={[
-                                { label: 'Grams', value: 'g' },
-                                { label: 'Centiliters', value: 'cl' },
-                            ]}
-                            my="md"
-                        />
-                        <Group position="right" mt="md">
-                            <Button variant="light" color="red" onClick={handleClose}>
-                                Cancel
-                            </Button>
-                            <Button ref={button} type="submit" variant="light" color="green">
-                                Create
-                            </Button>
-                        </Group>
-                    </form>
+                    <IngredientViewUpdate buttonText="Create" APICall={API.createIngredient} hanlder={validated}/>
                 </Modal.Body>
             </Modal.Content>
         </Modal.Root>
