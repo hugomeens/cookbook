0bc20b986a54b00c18360607e6ff6797e8eeb1ec
Xavier2p <xavier.de-place@epita.fr>
2023-05-29 17:39:46 +0200

    We can now change qty from recipe

--- client/src/pages/recipe/index.jsx
+++ client/src/pages/recipe/index.jsx
@@ -9,20 +9,24 @@ import setNotification from '../errors/error-notification';
 const Recipe = (item) => {
     const id = window.location.href.split('/').slice(-1)[0];
     const [recipe, setRecipe] = useState(null);
+    const [nbPerson, setNbPerson] = useState(0);
+
     useEffect(() => {
-        if (JSON.stringify(item) !== "{}") {
+        if (JSON.stringify(item) !== '{}') {
             setRecipe(item);
         } else {
             API.getRecipe(id)
                 .then((res) => {
                     if (res.status === 200) {
                         setRecipe(res.data);
+                        setNbPerson(res.data.nbPerson);
                     }
                 })
                 .catch((err) => {
                     setNotification(true, err);
                 });
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id]);
 
     return (
@@ -42,14 +46,14 @@ const Recipe = (item) => {
                     <Title order={3}>Ingredients</Title>
                     <Group position="right">
                         <Text>Number of people: </Text>
-                        <QuantityInput initialValue={recipe?.nbPerson ?? 0} />
+                        <QuantityInput initialValue={nbPerson} onChange={setNbPerson} />
                     </Group>
                 </Group>
                 <Grid columns={12}>
-                    {recipe?.ingredients?.length ?? 0 !== 0 ? (
+                    {(recipe?.ingredients?.length ?? 0) !== 0 ? (
                         recipe?.ingredients?.map((ingredient) => (
                             <Grid.Col xl={2} lg={3} md={3} sm={4} xs={4} key={ingredient?._id}>
-                                <IngredientsCard item={ingredient} />
+                                <IngredientsCard item={ingredient} nbPerson={nbPerson} defaultValue={recipe.nbPerson} />
                             </Grid.Col>
                         ))
                     ) : (
