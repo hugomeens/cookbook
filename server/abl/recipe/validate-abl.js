0ee85647c6b165e71f3ee3a1fbdca2be43d6551c
Samuel PERRIER <samuel.perrier@epita.fr>
2023-05-30 13:27:37 +0200

    don't validate recipe if an ingredient is not validate

--- server/abl/recipe/validate-abl.js
+++ server/abl/recipe/validate-abl.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { recipeDao } = require('../../dao/recipe-dao');
+const { ingredientDao } = require('../../dao/ingredient-dao');
 const { validateRecipeSchema } = require('../../schemas/recipe-schema');
 const Ajv = require('ajv').default;
 const { statusCodes } = require('../../utils/statusCodes');
@@ -13,11 +14,34 @@ async function ValidateAbl(body, res) {
     }
 
     try {
-        const mongoRes = await recipeDao.validate(body._id);
+        const mongoRes = await recipeDao.view(body._id);
         if (mongoRes.matchedCount == 0) {
             res.status(statusCodes.NOT_FOUND).json({ error: 'Recipe not found.' });
         } else {
-            res.status(statusCodes.OK).json({ _id: body._id });
+
+            console.log(mongoRes.ingredients);
+            const ingredientsInfo = await ingredientDao.view(mongoRes.ingredients);
+            console.log(ingredientsInfo);
+            let cpt = 0;
+            for (let i = 0; i < ingredientsInfo.length; i++) {
+                if (ingredientsInfo[i].valid === false) {
+                    cpt++;
+                }
+            }
+            console.log(cpt);
+            if (cpt === 0) {
+                mongoRes = await recipeDao.validate(body._id);
+                if (mongoRes.matchedCount == 0) {
+                    res.status(statusCodes.NOT_FOUND).json({ error: 'Recipe not found.' });
+                }
+                else {
+                    res.status(statusCodes.OK).json({ _id: body._id });
+                }
+            }
+            else {
+                res.status(statusCodes.PRECONDITION_FAILED).json({ error: 'All ingredients are not valid.' })
+            }
+
         }
     } catch (e) {
         res.status(statusCodes.INTERNAL_SERVER_ERROR).json({ error: e });
